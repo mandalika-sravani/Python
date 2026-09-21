22eb8e332c92ce24dade9aa5a065cfa5ac2d499e
sravani-mandalika <sravanimadalika@gmail.com>
2026-09-21 17:22:07 +0530

    added Variables

---
+++ Variables.py
@@ -0,0 +1,26 @@
+# Variable -- A container for a value (string, boolean, integer, float)
+#               A variable behaves as if it was the value it contains
+
+#Strings
+first_name = "Veditha"
+food = "tomato rice"
+email = "testingemail341@gmail.com"
+
+print(f"Hello {first_name}")
+print(f"I like {food}")
+print(f"This is my email: {email}")
+
+#Integers
+age = 25
+
+print(f"Age is {age}")
+
+#Float
+gpa = 7.9
+
+print(f"GPA is {gpa}")
+
+#Boolean
+isStudent = True
+
+print(f"Is {first_name} a student : {isStudent}")
