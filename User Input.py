4f1f37573a178d45a1c44b6511670186fea4e988
sravani-mandalika <sravanimadalika@gmail.com>
2026-09-21 18:10:45 +0530

    added User Inputs

---
+++ User Input.py
@@ -0,0 +1,19 @@
+# input() --- A function that prompts the user to enter data
+#               Returns the entered data as a string
+
+#name = input("What is your name?:")
+#age = int(input("How old are you?:"))
+
+#age = age + 1
+
+#print(f"Hello! {name}")
+#print(f"You are {age} years old")
+
+# Example 1
+
+length = float(input("Enter length:"))
+width = float(input("Enter width:"))
+
+area = length * width
+
+print(f"The area of rectanglr is : {area}")
