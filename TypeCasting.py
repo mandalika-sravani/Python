5291590359d4ad4344a2353b1a17ba47aa81cecb
sravani-mandalika <sravanimadalika@gmail.com>
2026-09-21 18:10:31 +0530

    added Type Casting

---
+++ TypeCasting.py
@@ -0,0 +1,16 @@
+# TypeCasting -- The process of converting a variable from one data type to another 
+#                   str(), int(), bool(), float()
+
+name = "Arjun"
+age = 16
+gpa = 3.9
+is_student = True
+
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(is_student))
+
+gpa = int(gpa)
+
+print(gpa)
