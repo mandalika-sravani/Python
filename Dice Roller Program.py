74edc0ca31e5bf5230be518bef198018db2cef8e
sravani-mandalika <sravanimadalika@gmail.com>
2026-09-23 18:25:27 +0530

    added dice roller program

--- Dice Roller Program.py
+++ Dice Roller Program.py
@@ -50,9 +50,14 @@ num_dice = int(input("How many dice? : "))
 for die in range(num_dice):
     dice.append(random.randint(1, 6))
 
-for die in range(num_dice):
-    for line in dice_art.get(dice[die]):
-        print(line)
+#for die in range(num_dice):
+#    for line in dice_art.get(dice[die]):
+#        print(line)
+
+for line in range(5):
+    for die in dice:
+        print(dice_art[die][line], end="  ")
+    print()   
 
 for die in dice:
     total += die
