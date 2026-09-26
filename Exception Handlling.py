0b5392cc8de57dbd8e02de6feed6826d7eec6a1e
sravani-mandalika <sravanimadalika@gmail.com>
2026-09-26 16:07:29 +0530

    added

---
+++ Exception Handlling.py
@@ -0,0 +1,14 @@
+# Exception ---An event that interrupts the flow of a program -- 1. try 2. except 3. finally
+
+try:
+    number = int(input("Enter a number : "))
+    print(1 / number)
+
+except ZeroDivisionError:
+    print("You can't divide by zero")
+except ValueError:
+    print("Enter only numbers")
+except Exception:
+    print("Something went wrong!")
+finally:
+    print("Do some cleanup")
